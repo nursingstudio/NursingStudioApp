aaa3bc3614de2561a6a6aeda81bf0a6622c75d6a
NursingStudio <nursingaddaapp@gmail.com>
2025-09-10 19:41:08 +0530

    1st step completed

--- app/src/main/java/com/example/nursingstudio/MainActivity.kt
+++ app/src/main/java/com/example/nursingstudio/MainActivity.kt
@@ -1,20 +1,81 @@
 package com.example.nursingstudio
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var topAppBar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Find Views
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+        topAppBar = findViewById(R.id.topAppBar)
+
+        // Top bar navigation button (hamburger menu)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
         }
+
+        // Drawer item clicks
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_subscription -> { /* TODO */ }
+                R.id.nav_notice -> { /* TODO */ }
+                R.id.nav_social -> { /* TODO */ }
+                R.id.nav_share -> { /* TODO */ }
+                R.id.nav_settings -> { /* TODO */ }
+            }
+            drawerLayout.close()
+            true
+        }
+
+        // Bottom navigation clicks
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_quiz -> {
+                    loadFragment(QuizFragment())
+                    true
+                }
+                R.id.nav_pdf -> {
+                    loadFragment(PdfFragment())
+                    true
+                }
+                R.id.nav_video -> {
+                    loadFragment(VideoFragment())
+                    true
+                }
+                R.id.nav_mypage -> {
+                    loadFragment(MyPageFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Default fragment
+        if (savedInstanceState == null) {
+            loadFragment(QuizFragment())
+            bottomNavigation.selectedItemId = R.id.nav_quiz
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
